ccf13907146d6e146ccfa97e63e06879cb95eed9
xpercy29 <spangeled@gmail.com>
2023-12-20 03:20:16 +0530

    count vowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -3,10 +3,23 @@
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
   Once you've implemented the logic, test your code by running
+
+  https://chat.openai.com/share/c6d7681a-51ef-46ea-a992-e6886b55ecca
 */
 
 function countVowels(str) {
-    // Your code here
+      // Array of vowels to check against
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  // Convert the input string to lowercase to consider both uppercase and lowercase vowels
+  const lowerCaseStr = str.toLowerCase();
+
+  // Use Array.from to convert the string into an array of characters,
+  // then filter out the vowels and return the count
+  const vowelCount = Array.from(lowerCaseStr).filter((char) => vowels.includes(char)).length;
+
+  return vowelCount;
+
 }
 
 module.exports = countVowels;
